82a93186bf64bdaf56a639319252f5fa4ceaba1b
Kevin Duzguner <67980369+kevinbaddata@users.noreply.github.com>
2022-08-22 21:54:13 +0200

    Add files via upload

---
+++ devices.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    struct ifaddrs* address; 
+    
+    if(getifaddrs(&address) == -1)
+    {   
+        // error
+        perror("getifaddrs");
+        exit(1);
+    //...
+    }
+    //...
+
+    while(address != NULL)
+    {
+        int family = address->ifa_addr->sa_family;
+
+        if(family == AF_INET || family == AF_INET6)
+        {
+            printf("%s: ", address->ifa_name);
+            printf("%s", family == AF_INET ? "IPv4" : "IPv6");
+
+            char ap[100];
+
+            const int family_size = family == AF_INET ? 
+            sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+            getnameinfo(address->ifa_addr,
+            family_size, ap, sizeof(ap), 0, 0, 0x00000002);
+            printf("\t%s\n", ap);
+        }
+
+        address = address->ifa_next;
+    }
+
+    //release memory...
+    freeifaddrs(address);
+
+    return 0;
+}
